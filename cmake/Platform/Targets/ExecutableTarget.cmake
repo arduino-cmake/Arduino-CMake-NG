261c06edf5087c9c467cb3e1b23a8bd1f2a3bb42
Timor Gruber <timor.gruber@gmail.com>
2018-06-01 22:01:12 +0300

    Added function to setup bootloader arguments for uploading firmwares to boards.

--- cmake/Platform/Targets/ExecutableTarget.cmake
+++ cmake/Platform/Targets/ExecutableTarget.cmake
@@ -1,9 +1,11 @@
-function(add_arduino_executable a_target_name _board_id _src_files)
+function(add_arduino_executable _target_name _board_id _src_files)
 
     add_executable(${_target_name} "${_src_files}")
+    #[[target_include_directories(${_target_name}
+            "${ARDUINO_CMAKE_CORE_${ARDUINO_CMAKE_PLATFORM_DEFAULT_CORE}_PATH}")]]
 
     # Create EEP object file from build's ELF object file
-    add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
+    add_custom_command(TARGET ${_target_name} POST_BUILD
             COMMAND ${CMAKE_OBJCOPY}
             ARGS ${ARDUINO_CMAKE_OBJCOPY_EEP_FLAGS}
             ${TARGET_PATH}.elf
@@ -12,7 +14,7 @@ function(add_arduino_executable a_target_name _board_id _src_files)
             VERBATIM)
 
     # Convert firmware image to ASCII HEX format
-    add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
+    add_custom_command(TARGET ${_target_name} POST_BUILD
             COMMAND ${CMAKE_OBJCOPY}
             ARGS ${ARDUINO_CMAKE_OBJCOPY_HEX_FLAGS}
             ${TARGET_PATH}.elf
--- cmake/Platform/Targets/UploadTarget.cmake
+++ cmake/Platform/Targets/UploadTarget.cmake
@@ -1,13 +1,43 @@
-function(upload_arduino_target _target_name _board_id)
+function(_setup_bootloader_arguments _target_name _board_id _port _return_var)
+
+    set(avrdude_flags ${ARDUINO_CMAKE_AVRDUDE_FLAGS})
+
+    get_board_property(${_board_id} build.mcu board_mcu)
+    if (NOT "${board_mcu}" STREQUAL "") # MCU is found
+        list(APPEND avrdude_flags "-p${board_mcu}")
+    endif ()
+
+    get_board_property(${_board_id} upload.protocol board_upload_protocol)
+    if (NOT "${board_upload_protocol" STREQUAL "") # Upload protocol is found
+        if ("${board_upload_protocol}" STREQUAL "stk500")
+            set(board_upload_protocol "stk500v1")
+        endif ()
+        list(APPEND avrdude_flags "-c${board_upload_protocol}")
+    endif ()
+
+    get_board_property(${_board_id} upload.speed board_upload_speed)
+    if (NOT "${board_upload_speed}" STREQUAL "") # Speed is found
+        list(APPEND avrdude_flags "-b${board_upload_speed}")
+    endif ()
+
+    list(APPEND avrdude_flags "-P${_port}" "-D") # Upload port, don't erase
+    list(APPEND avrdude_flags "-C${ARDUINO_CMAKE_AVRDUDE_CONFIG_PATH}") # Avrdude config file
+
+    set(${_return_var} ${avrdude_flags} PARENT_SCOPE)
+
+endfunction()
+
+function(upload_arduino_target _target_name _board_id _port)
 
     if (NOT ${_target_name})
         message(FATAL_ERROR "Can't create upload target for an invalid target ${_target_name}")
     endif ()
 
-    add_custom_command(${BOOTLOADER_TARGET}
-            ${ARDUINO_CMAKE_AVRDUDE_PROGRAM}
-            ${AVRDUDE_ARGS}
-            WORKING_DIRECTORY ${ARDUINO_CMAKE_BOOTLOADERS_PATH}/${BOOTLOADER_PATH}
+    _setup_bootloader_arguments(${_target_name} ${_board_id} ${_port} upload_args)
+
+    add_custom_command(COMMAND ${ARDUINO_CMAKE_AVRDUDE_PROGRAM}
+            ARGS ${upload_args}
+            COMMENT "Uploading ${_target_name} target"
             DEPENDS ${_target_name})
 
 endfunction()
