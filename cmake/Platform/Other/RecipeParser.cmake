e9298a68aa06910fcfcbb0682841fb4b5e19d6a8
Timor Gruber <timor.gruber@gmail.com>
2018-06-04 08:10:40 +0300

    Modified compiler-recipe function to filter list with unwanted patterns before processing it, leading to much cleaner results.

--- cmake/Platform/Other/RecipeParser.cmake
+++ cmake/Platform/Other/RecipeParser.cmake
@@ -3,22 +3,16 @@ include(RecipePropertyValueResolver)
 function(parse_compiler_recipe_flags _board_id _return_var)
 
     set(recipe_language cpp) # Use C++ by default
+    set(original_list "${recipe_cpp_o_pattern}")
     set(final_recipe "")
 
-    foreach (recipe_element ${recipe_cpp_o_pattern})
-        string(REGEX MATCH "path|cmd" non_flag_element "${recipe_element}")
-        if ("${non_flag_element}" STREQUAL "") # Element IS a flag, parse it
-            # Element is wrapped with brackets = hasn't been resolved earlier and should be ommited
-            string(REGEX MATCH "^[{\"].+[}\"]$" unresolved_element "${recipe_element}")
-            if (NOT "${unresolved_element}" STREQUAL "") # Element is unresolved but should be
-                continue()
-            endif ()
+    # Filter unwanted patterns from the recipe, so that only wanted ones will be parsed
+    list(FILTER original_list INCLUDE REGEX "(^[^\"].+[^\"]$)")
 
-            # Resolve element by searching it in the board's properties
-            _resolve_recipe_property("${recipe_element}" resolved_element)
-            if (NOT "${resolved_element}" STREQUAL "")
-                list(APPEND final_recipe "${resolved_element}")
-            endif ()
+    foreach (recipe_element ${recipe_cpp_o_pattern})
+        _resolve_recipe_property("${recipe_element}" resolved_element)
+        if (NOT "${resolved_element}" STREQUAL "") # Unresolved element, don't append
+            list(APPEND final_recipe "${resolved_element}")
         endif ()
     endforeach ()
 
