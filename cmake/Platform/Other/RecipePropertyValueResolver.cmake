cfbdb74419d1f51ecb5278b5c40f3e3ff5ecccca
Timor Gruber <timor.gruber@gmail.com>
2018-06-04 07:34:55 +0300

    Refactored project structure by moving property readers under 'Other' directory.

---
+++ cmake/Platform/Other/RecipePropertyValueResolver.cmake
@@ -0,0 +1,38 @@
+function(_get_recipe_property_name _property _return_var)
+
+    string(REGEX MATCH "^[^{]+" property_name "${_property}")
+    set(${_return_var} "${property_name}" PARENT_SCOPE)
+
+endfunction()
+
+function(_get_recipe_property_value _property _return_var)
+
+    string(STRIP "${_property}" stripped_property)
+    string(REGEX MATCH "{.+}$" property_value "${stripped_property}")
+    set(${_return_var} "${property_value}" PARENT_SCOPE)
+
+endfunction()
+
+function(_resolve_recipe_property _property _return_var)
+
+    string(REGEX MATCH "=" normal_property "${_property}")
+    if ("${normal_property}" STREQUAL "") # Recipe property, doesn't have '='
+        _get_recipe_property_name("${_property}" property_name)
+        _get_recipe_property_value("${_property}" property_value)
+        _resolve_value("${property_value}" resolved_property_value ${_board_id})
+        set(resolved_property "${property_name}${resolved_property_value}")
+    else ()
+        _get_property_name("${_property}" property_name)
+        _get_property_value("${_property}" property_value)
+        _resolve_value("${property_value}" resolved_property_value ${_board_id})
+        set(resolved_property "${property_name}=${resolved_property_value}")
+    endif ()
+
+    # If value couldn't been resolved, return empty string
+    if ("${resolved_property_value}" STREQUAL "")
+        set(${_return_var} "" PARENT_SCOPE)
+    else ()
+        set(${_return_var} "${resolved_property}" PARENT_SCOPE)
+    endif ()
+
+endfunction()
