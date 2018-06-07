ad95f48607c7aa219169b98e3d7ea3319bfff55d
Timor Gruber <timor.gruber@gmail.com>
2018-06-07 23:28:15 +0300

    Refactored upload flags setting in the `UploadTarget` module to be managed by the `TargetFlagsManager` module.

--- cmake/Platform/Other/TargetFlagsManager.cmake
+++ cmake/Platform/Other/TargetFlagsManager.cmake
@@ -4,3 +4,20 @@ function(set_executable_target_flags _target_name _board_id)
     target_compile_options(${_target_name} PUBLIC ${parsed_recipe})
 
 endfunction()
+
+function(set_upload_target_flags _target_name _board_id _upload_port _return_var)
+
+    set(upload_flags "")
+
+    # Parse and append recipe flags
+    parse_upload_recipe_pattern(${_board_id} ${_upload_port} upload_recipe_flags)
+    list(APPEND upload_flags "${upload_recipe_flags}")
+
+    set(target_binary_base_path "${CMAKE_CURRENT_BINARY_DIR}/${_target_name}")
+
+    list(APPEND upload_flags "-Uflash:w:\"${target_binary_base_path}.hex\":i")
+    list(APPEND upload_flags "-Ueeprom:w:\"${target_binary_base_path}.eep\":i")
+
+    set(${_return_var} ${upload_flags} PARENT_SCOPE)
+
+endfunction()
--- cmake/Platform/Targets/UploadTarget.cmake
+++ cmake/Platform/Targets/UploadTarget.cmake
@@ -1,27 +1,10 @@
-function(_setup_bootloader_arguments _board_id _port _return_var)
-
-    set(avrdude_flags "")
-
-    # Parse and append recipe flags
-    parse_upload_recipe_pattern(${_board_id} ${_port} upload_recipe_flags)
-    list(APPEND avrdude_flags "${upload_recipe_flags}")
-
-    message("Bootloader args: ${avrdude_flags}")
-
-    set(${_return_var} ${avrdude_flags} PARENT_SCOPE)
-
-endfunction()
-
 function(upload_arduino_target _target_name _board_id _port)
 
     if ("${_target_name}" STREQUAL "")
         message(FATAL_ERROR "Can't create upload target for an invalid target ${_target_name}")
     endif ()
 
-    _setup_bootloader_arguments("${_board_id}" ${_port} upload_args)
-    set(target_binary_base_path "${CMAKE_CURRENT_BINARY_DIR}/${_target_name}")
-    list(APPEND upload_args "-Uflash:w:\"${target_binary_base_path}.hex\":i")
-    list(APPEND upload_args "-Ueeprom:w:\"${target_binary_base_path}.eep\":i")
+    set_upload_target_flags("${_target_name}" ${_board_id} "${_port}" upload_args)
 
     add_custom_command(TARGET ${_target_name} POST_BUILD
             COMMAND ${ARDUINO_CMAKE_AVRDUDE_PROGRAM}
