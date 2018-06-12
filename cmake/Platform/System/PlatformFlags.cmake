b7152cfdfd3756a8bf7fbf3f8070b1a669f4783b
Timor Gruber <timor.gruber@gmail.com>
2018-06-13 00:10:34 +0300

    Added support for "custom" ar-flags defined by the platform file.

--- cmake/Platform/System/PlatformFlags.cmake
+++ cmake/Platform/System/PlatformFlags.cmake
@@ -1,3 +1,18 @@
 function(setup_remaining_platform_flags)
+
     set(ARDUINO_CMAKE_AVRDUDE_FLAGS ${tools_avrdude_upload_params_verbose})
+
+    # Set AR flags based on the platform file
+    if (compiler_ar_flags)
+        set(CMAKE_ASM_ARCHIVE_CREATE
+                "<CMAKE_AR> ${compiler_ar_flags} <TARGET> <LINK_FLAGS> <OBJECTS>"
+                CACHE STRING "" FORCE)
+        set(CMAKE_C_ARCHIVE_CREATE
+                "<CMAKE_AR> ${compiler_ar_flags} <TARGET> <LINK_FLAGS> <OBJECTS>"
+                CACHE STRING "" FORCE)
+        set(CMAKE_CXX_ARCHIVE_CREATE
+                "<CMAKE_AR> ${compiler_ar_flags} <TARGET> <LINK_FLAGS> <OBJECTS>"
+                CACHE STRING "" FORCE)
+    endif ()
+
 endfunction()
