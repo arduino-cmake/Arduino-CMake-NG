ab7cb1d88d3f9f845017768b545e4c3c9d4eb256
Timor Gruber <timor.gruber@gmail.com>
2018-07-04 23:24:51 +0300

    Refactored 'find_dependent_platform_libraries' function into separate functions.

--- cmake/Platform/Other/ArduinoLibraryParser.cmake
+++ cmake/Platform/Other/ArduinoLibraryParser.cmake
@@ -1,3 +1,20 @@
+function(_get_platform_libraries_from_includes _include_locs _return_var)
+
+    set(platform_libs)
+    foreach (include ${_include_locs})
+        string(REGEX REPLACE "[\"<](.+)\\." "\\1" include_name ${include})
+        string(TOLOWER ${include_name} include_name)
+        if (${include_name} IN_LIST ARDUINO_CMAKE_PLATFORM_LIBRARIES)
+            list(APPEND platform_libs ${include_name})
+        else ()
+            continue()
+        endif ()
+    endforeach ()
+
+    set(${_return_var} ${platform_libs} PARENT_SCOPE)
+
+endfunction()
+
 function(find_dependent_platform_libraries _return_var)
 
     set(sources ${ARGN})
@@ -8,18 +25,8 @@ function(find_dependent_platform_libraries _return_var)
         list(APPEND includes ${source_includes})
     endforeach ()
 
-    set(dependent_libs)
     list(REMOVE_DUPLICATES includes)
-    foreach (include ${includes})
-        string(REGEX REPLACE "[\"<](.+)\\." "\\1" include_name ${include})
-        string(TOLOWER ${include_name} include_name)
-        list(FIND ARDUINO_CMAKE_PLATFORM_LIBRARIES ${include_name} platform_lib_index)
-        if (${platform_lib_index} LESS 0)
-            continue()
-        else ()
-            list(APPEND dependent_libs ${include_name})
-        endif ()
-    endforeach ()
+    _get_platform_libraries_from_includes("${includes}" dependent_libs)
 
     set(${_return_var} ${dependent_libs} PARENT_SCOPE)
 
