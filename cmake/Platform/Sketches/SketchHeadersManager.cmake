729fe950d48df20165f930cb8e125c66f4e86b99
Timor Gruber <timor.gruber@gmail.com>
2018-08-26 21:36:39 +0300

    Refactored header-resolving logic from `add_sketch_to_target` to separate function under `SketchHeadersManager` module. Documented the entire module as well.

--- cmake/Platform/Sketches/SketchHeadersManager.cmake
+++ cmake/Platform/Sketches/SketchHeadersManager.cmake
@@ -1,4 +1,11 @@
-function(get_sketch_headers _sketch_file _return_var)
+#=============================================================================#
+# Retrieves all headers used by a sketch, which is much like extracting the headers included
+# by a source file. Headers are returned by their name, with extension (such as '.h').
+#       _sketch_file - Path to a sketch file to add to the target.
+#       _return_var - Name of variable in parent-scope holding the return value.
+#       Returns - List of headers names with extension that are included by the given sketch file.
+#=============================================================================#
+function(_get_sketch_headers _sketch_file _return_var)
 
     file(STRINGS "${_sketch_file}" sketch_loc) # Loc = Lines of code
     list(FILTER sketch_loc INCLUDE REGEX ${ARDUINO_CMAKE_HEADER_INCLUDE_REGEX_PATTERN})
@@ -13,7 +20,14 @@ function(get_sketch_headers _sketch_file _return_var)
 
 endfunction()
 
-function(validate_header_against_target _target_name _header _return_var)
+#=============================================================================#
+# Validates a header file is included by the given target.
+# i.e The header is located under one of the target's include directories.
+#       _target_name - Name of the target to add the sketch file to.
+#       _return_var - Name of variable in parent-scope holding the return value.
+#       Returns - True if header is included by the target, false otherwise.
+#=============================================================================#
+function(_validate_target_includes_header _target_name _header _return_var)
 
     get_target_property(target_include_dirs ${_target_name} INCLUDE_DIRECTORIES)
     foreach (include_dir ${target_include_dirs})
@@ -25,5 +39,43 @@ function(validate_header_against_target _target_name _header _return_var)
     endforeach ()
 
     set(${_return_var} False)
+
+endfunction()
+
+#=============================================================================#
+# Resolves the header files included in a sketch by linking their appropriate library if necessary
+# or by validating they're included by the sketch target.
+#       _target_name - Name of the target to add the sketch file to.
+#       _sketch_file - Path to a sketch file to add to the target.
+#       _board_id - ID of the board to bind to the target (Each target can have a single board).
+#=============================================================================#
+function(resolve_sketch_headers _target_name _sketch_file _board_id)
+
+    _get_sketch_headers("${_sketch_file}" sketch_headers)
+    foreach (header ${sketch_headers})
+        # Header name without extension (such as '.h') can represent an Arduino/Platform library
+        # So first we should check whether it's a library
+        string(REGEX MATCH "(.+)\\." "${header}" header_we_match)
+        set(header_we ${CMAKE_MATCH_1})
+
+        if (${header_we} IN_LIST ARDUINO_CMAKE_PLATFORM_LIBRARIES)
+            link_platform_library(${_target_name} ${header_we} ${_board_id})
+        else ()
+            find_arduino_library(${header_we}_sketch_lib ${header_we} ${_board_id})
+            # If library isn't found, display a wraning since it might be a user library
+            if (NOT ${header_we}_sketch_lib OR "${${header_we}_sketch_lib}" MATCHES "NOTFOUND")
+                _validate_target_includes_header(${_target_name} ${header} is_header_validated)
+                if (NOT is_header_validated)
+                    # Header hasn't been found in any of the target's include directories, Display warning
+                    message(WARNING "The header '${_header}' is used by the \
+                                     '${_sketch_file}' sketch \
+                                     but it isn't a Arduino/Platform library, nor it's linked \
+                                     to the target manually!")
+                endif ()
+            else ()
+                link_arduino_library(${_target_name} ${header_we}_sketch_lib ${_board_id})
+            endif ()
+        endif ()
+    endforeach ()
 
 endfunction()
--- cmake/Platform/Sketches/SketchManager.cmake
+++ cmake/Platform/Sketches/SketchManager.cmake
@@ -5,7 +5,7 @@ include(SketchHeadersManager)
 # Returns a desired path for sources converted from sketches.
 # It can't be resolved just by a cache variable since sketches may belong each to a different project,
 # thus having different path to be returned.
-#       _sketch_file - Path to a sketch file to find the desired path to its converted source
+#       _sketch_file - Path to a sketch file to find the desired path to its converted source.
 #       _return_var - Name of variable in parent-scope holding the return value.
 #       Returns - Desired path for the source file converted from the given sketch
 #=============================================================================#
@@ -21,39 +21,13 @@ endfunction()
 # Adds the sketch file to the given target with the given board ID.
 # Each sketch is converted to a valid '.cpp' source file under the project's source directory.
 # The function also finds and links any libraries the sketch uses to the target.
-#       _target_name - Name of the target to add the sketch file to
-#       _sketch_file - Path to a sketch file to add to the target
+#       _target_name - Name of the target to add the sketch file to.
+#       _sketch_file - Path to a sketch file to add to the target.
 #       _board_id - ID of the board to bind to the target (Each target can have a single board).
 #=============================================================================#
 function(add_sketch_to_target _target_name _sketch_file _board_id)
 
-    get_sketch_headers("${_sketch_file}" sketch_headers)
-    foreach (header ${sketch_headers})
-        # Header name without extension (such as '.h') can represent an Arduino/Platform library
-        # So first we should check whether it's a library
-        string(REGEX MATCH "(.+)\\." "${header}" header_we_match)
-        set(header_we ${CMAKE_MATCH_1})
-
-        if (${header_we} IN_LIST ARDUINO_CMAKE_PLATFORM_LIBRARIES)
-            link_platform_library(${_target_name} ${header_we} ${_board_id})
-        else ()
-            find_arduino_library(${header_we}_sketch_lib ${header_we} ${_board_id})
-            # If library isn't found, display a wraning since it might be a user library
-            if (NOT ${header_we}_sketch_lib OR "${${header_we}_sketch_lib}" MATCHES "NOTFOUND")
-                validate_header_against_target(${_target_name} ${header} is_header_validated)
-                if (NOT is_header_validated)
-                    # Header hasn't been found in any of the target's include directories, Display warning
-                    message(WARNING "The header '${_header}' is used by the \
-                                     '${_sketch_file}' sketch \
-                                     but it isn't a Arduino/Platform library, nor it's linked \
-                                     to the target manually!")
-                endif ()
-            else ()
-                link_arduino_library(${_target_name} ${header_we}_sketch_lib ${_board_id})
-            endif ()
-        endif ()
-    endforeach ()
-
+    resolve_sketch_headers(${_target_name} "${_sketch_file}")
     _get_converted_source_desired_path("${_sketch_file}" sketch_converted_source_path)
     convert_sketch_to_source("${_sketch_file}" "${sketch_converted_source_path}")
     target_sources(${_target_name} PRIVATE "${sketch_converted_source_path}")
