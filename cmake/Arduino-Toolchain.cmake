b45b0dc3b4365e52d16869fcbc93db2a1080bc1e
Timor Gruber <timor.gruber@gmail.com>
2018-09-06 21:24:46 +0300

    Improved env-var approach by fixing subtle bugs.

--- cmake/Arduino-Toolchain.cmake
+++ cmake/Arduino-Toolchain.cmake
@@ -5,26 +5,32 @@ function(_find_required_programs)
     # Find ASM compiler
     find_program(CMAKE_ASM_COMPILER avr-gcc
             PATHS ${ARDUINO_SDK_BIN_PATH}
+            NO_DEFAULT_PATH
             NO_CMAKE_FIND_ROOT_PATH)
     # Find C compiler
     find_program(CMAKE_C_COMPILER avr-gcc
             PATHS ${ARDUINO_SDK_BIN_PATH}
+            NO_DEFAULT_PATH
             NO_CMAKE_FIND_ROOT_PATH)
     # Find C++ compiler
     find_program(CMAKE_CXX_COMPILER avr-g++
             PATHS ${ARDUINO_SDK_BIN_PATH}
+            NO_DEFAULT_PATH
             NO_CMAKE_FIND_ROOT_PATH)
     # Find AR required for linkage
     find_program(CMAKE_AR avr-gcc-ar
             PATHS ${ARDUINO_SDK_BIN_PATH}
+            NO_DEFAULT_PATH
             NO_CMAKE_FIND_ROOT_PATH)
     # Find Ranlib required for linkage
     find_program(CMAKE_RANLIB avr-gcc-ranlib
             PATHS ${ARDUINO_SDK_BIN_PATH}
+            NO_DEFAULT_PATH
             NO_CMAKE_FIND_ROOT_PATH)
     # Find NM
     find_program(CMAKE_NM avr-gcc-nm
             PATHS ${ARDUINO_SDK_BIN_PATH}
+            NO_DEFAULT_PATH
             NO_CMAKE_FIND_ROOT_PATH)
 
 endfunction()
@@ -52,12 +58,11 @@ endif ()
 set(ARDUINO_CMAKE_TOOLCHAIN_DIR ${CMAKE_CURRENT_LIST_DIR} CACHE PATH
         "Path to Arduino-CMake's toolchain directory")
 
-message("SDK: $ENV{ARDUINO_SDK_PATH}")
-
-# Set default path if none is set
-if ($ENV{ARDUINO_SDK_PATH})
-    set(ARDUINO_SDK_PATH "${ARDUINO_SDK_PATH}" CACHE PATH "Arduino SDK Path")
+if (DEFINED ENV{ARDUINO_SDK_PATH})
+    string(REPLACE "\\" "/" unix_style_sdk_path $ENV{ARDUINO_SDK_PATH})
+    set(ARDUINO_SDK_PATH "${unix_style_sdk_path}" CACHE PATH "Arduino SDK Path")
 else ()
+    # Set default path if none is set
     find_arduino_sdk(arduino_sdk_path)
     set(ARDUINO_SDK_PATH "${arduino_sdk_path}" CACHE PATH "Arduino SDK Path")
 endif ()
