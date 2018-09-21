0831a0d9176a7313ae6a03b720c80f467f2619df
Timor Gruber <timor.gruber@gmail.com>
2018-09-21 22:38:52 +0300

    Modified executable API to treat source files as unparsed arguments. It means that there's no longer a variable that accepts sources, however, it's invisible to the user. Fixed a few bugs in the '3rd-party-library' example, found out after enabling the new API.
    Signed-off-by: Timor Gruber <timor.gruber@gmail.com>

--- examples/3rd-party-library/NeoPixelTest.cpp
+++ examples/3rd-party-library/NeoPixelTest.cpp
@@ -1,10 +1,12 @@
 #include <Adafruit_NeoPixel.h>
+#include "GFXTest.h"
 
 Adafruit_NeoPixel neoPixel;
 
 void setup()
 {
     neoPixel.clear();
+    doSomething();
 }
 
 void loop()
---
+++ examples/3rd-party-library/include/GFXTest.h
@@ -0,0 +1,8 @@
+#ifndef EXAMPLES_GFXTEST_HPP
+#define EXAMPLES_GFXTEST_HPP
+
+#include <Adafruit_GFX.h>
+
+void doSomething();
+
+#endif //EXAMPLES_GFXTEST_HPP
