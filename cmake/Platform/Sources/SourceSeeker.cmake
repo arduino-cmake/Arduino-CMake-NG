ee58a0cdd1b991454545f9df034a1b106e1bdbff
Timor Gruber <timor.gruber@gmail.com>
2018-09-21 15:47:39 +0300

    Added feature to add all sources inside list of directories to target. It mostly allows users to append additional directories of a library to their library target, since now libraries only search in the 'src' sub-directory or root directory by default, and this behavior can't be changed. Also separated source-finding function related to libraries to a separate module. Updated '3rd-party-library' example to use new API.

--- cmake/Platform/Sources/SourceSeeker.cmake
+++ cmake/Platform/Sources/SourceSeeker.cmake
@@ -56,46 +56,6 @@ function(find_source_files _base_path _return_var)
 endfunction()
 
 #=============================================================================#
-# Recursively finds header files under the given path, excluding those that don't belong to a library,
-# such as files under the 'exmaples' directory (In case sources reside under lib's root directory).
-#        _base_path - Top-Directory path to search source files in.
-#        _return_var - Name of variable in parent-scope holding the return value.
-#        Returns - List of source files in the given path
-#=============================================================================#
-function(find_library_header_files _base_path _return_var)
-
-    if (EXISTS ${_base_path}/src) # 'src' sub-dir exists and should contain sources
-        # Headers are always searched recursively under the 'src' sub-dir
-        find_header_files(${_base_path}/src headers RECURSE)
-    else ()
-        find_header_files(${_base_path} headers)
-    endif ()
-
-    set(${_return_var} "${headers}" PARENT_SCOPE)
-
-endfunction()
-
-#=============================================================================#
-# Recursively finds source files under the given path, excluding those that don't belong to a library,
-# such as files under the 'exmaples' directory (In case sources reside under lib's root directory).
-#        _base_path - Top-Directory path to search source files in.
-#        _return_var - Name of variable in parent-scope holding the return value.
-#        Returns - List of source files in the given path
-#=============================================================================#
-function(find_library_source_files _base_path _return_var)
-
-    if (EXISTS ${_base_path}/src)
-        # Sources are always searched recursively under the 'src' sub-dir
-        find_source_files(${_base_path}/src sources RECURSE)
-    else ()
-        find_source_files(${_base_path} sources)
-    endif ()
-
-    set(${_return_var} "${sources}" PARENT_SCOPE)
-
-endfunction()
-
-#=============================================================================#
 # Finds sketch files matching the pre-defined sketch-file pattern under the given path.
 # This functions searchs explicitly for sketch-files such as '*.ino'.
 # Search could also be recursive (With sub-directories) if the optional 'RECURSE' option is passed.
--- examples/3rd-party-library/testGFX.cpp
+++ examples/3rd-party-library/testGFX.cpp
@@ -1,12 +1,9 @@
-#include <Adafruit_NeoPixel.h>
 #include <Adafruit_GFX.h>
 
-Adafruit_NeoPixel neoPixel;
 Adafruit_GFX_Button gfxButton;
 
 void setup()
 {
-    neoPixel.clear();
     gfxButton.isPressed();
 }
 
