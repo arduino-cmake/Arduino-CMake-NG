76fcb99a95da2710fb2b212a93ded4d82fde8e49
Timor Gruber <timor.gruber@gmail.com>
2018-09-24 14:41:19 +0300

    Removed 'EnableInterrupt' 3rd-party library from example as it's badly written.

--- examples/3rd-party-library/3rd_party.cpp
+++ examples/3rd-party-library/3rd_party.cpp
@@ -1,13 +1,11 @@
 #include <Arduino.h>
 #include "NeoPixelTest.hpp"
 #include "GFXTest.h"
-#include "InterruptTest.hpp"
 
 void setup()
 {
     testNeoPixel();
     testGFX();
-    testEnableInterrupt();
 }
 
 void loop()
