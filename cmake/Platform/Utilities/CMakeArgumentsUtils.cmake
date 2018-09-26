83ca4656850ead9b2aa6c8496d27821cb92caf43
Timor Gruber <timor.gruber@gmail.com>
2018-09-26 03:16:42 +0300

    Added utility function to parse sources from cmake's unparsed arguments. It allows functions to accept practically unlimited sources (or any other arbitrary value) as long as any reserved arguments/options aren't encountered. When at least one is encountered, processing stops and the already-parsed arguments are returned.

--- cmake/Platform/Utilities/CMakeArgumentsUtils.cmake
+++ cmake/Platform/Utilities/CMakeArgumentsUtils.cmake
@@ -1,4 +1,48 @@
 #=============================================================================#
+# Parses the given arguments for sources, stopping when all arguments have been read or
+# when at least one reserved argument/option has been encountered.
+#       _reserved_options - Reserved option arguments.
+#       _reserved_single_values - Reserved single-value arguments.
+#       _reserved_multi_values - Reserved multi-value arguments.
+#       _cmake_args - Arguments to parse. Usually function's unparsed arguments - ${ARGN}.
+#       _return_var - Name of a CMake variable that will hold the extraction result.
+#       Returns - Parsed sources.
+#=============================================================================#
+function(parse_sources_arguments _return_var _reserved_options _reserved_single_values
+        _reserved_multi_values _cmake_args)
+
+    # Prepare arguments for further inspection - Somewhat croocked logic due to CMake's limitations
+    # If an argument is an empty string, populate it with a theoritcally impossible value.
+    # just to have some value in it
+    if ("${_reserved_options}" STREQUAL "")
+        set(_reserved_options "+-*/")
+    endif ()
+    if ("${_reserved_single_values}" STREQUAL "")
+        set(_reserved_single_values "+-*/")
+    endif ()
+    if ("${_reserved_multi_values}" STREQUAL "")
+        set(_reserved_multi_values "+-*/")
+    endif ()
+
+    set(sources "") # Clear list because cmake preserves scope in nested functions
+
+    foreach (arg ${_cmake_args})
+
+        if (${arg} IN_LIST _reserved_options OR
+                ${arg} IN_LIST _reserved_single_values OR
+                ${arg} IN_LIST _reserved_multi_values)
+            break()
+        else ()
+            list(APPEND sources ${arg})
+        endif ()
+
+    endforeach ()
+
+    set(${_return_var} ${sources} PARENT_SCOPE)
+
+endfunction()
+
+#=============================================================================#
 # Parses the given arguments for scope-controlling arguments, which are PUBLIC|INTERFACE|PRIVATE.
 # If none is found, CMake generates an error, unless - The DEFAULT_SCOPE argument is passed.
 #       _cmake_args - Arguments to parse. Usually function's unparsed arguments - ${ARGN}.
