ad218e720ac21ad7fa8c59ceb7ee83c8245dacfe
Timor Gruber <timor.gruber@gmail.com>
2018-10-06 16:07:38 +0300

    Fixed bug causing single-supported architecture libraries to fail. Also added the 3rd party library 'LiquidCrystal_I2C' which has an inner dependency on the 'Wire' platform library.

---
+++ examples/3rd-party-library/include/LiquidCrystalTest.hpp
@@ -0,0 +1,13 @@
+#ifndef EXAMPLES_LIQUIDCRYSTALTEST_HPP
+#define EXAMPLES_LIQUIDCRYSTALTEST_HPP
+
+#include <LiquidCrystal_I2C.h>
+#include <Arduino.h>
+
+#define LCD_ADDRESS     0x27
+#define LCD_COLUMNS     20
+#define LCD_ROWS        4
+
+void testLiquidCrystal();
+
+#endif //EXAMPLES_LIQUIDCRYSTALTEST_HPP
