11399f228fbe77d6e6c6272b311fca4f54cbceec
Timor Gruber <timor.gruber@gmail.com>
2019-04-05 22:09:24 +0300

    Upgraded header discoverability (by targets) check

--- cmake/Platform/Sources/HeaderExistanceChecker.cmake
+++ cmake/Platform/Sources/HeaderExistanceChecker.cmake
@@ -8,32 +8,14 @@
 #=============================================================================#
 function(is_header_discoverable_by_target _header_we _target_name _return_var)
 
-    # Get target's direct include dirs
-    get_target_property(target_include_dirs ${_target_name} INCLUDE_DIRECTORIES)
+    get_target_include_directories(${_target_name} target_include_dirs)
 
-    # Get include dirs of targets linked to the given target
-    get_target_property(target_linked_libs ${_target_name} LINK_LIBRARIES)
+    get_header_file(${_header_we} ${target_include_dirs} header_found)
 
-    # Explictly add include dirs of all linked libraries (given they're valid cmake targets)
-    foreach (linked_lib ${target_linked_libs})
-
-        if (NOT TARGET ${linked_lib})
-            continue()
-        endif ()
-
-        get_target_property(lib_include_dirs ${linked_lib} INCLUDE_DIRECTORIES)
-        if (NOT "${lib_include_dirs}" MATCHES "NOTFOUND") # Library has include dirs
-            list(APPEND include_dirs ${lib_include_dirs})
-        endif ()
-
-    endforeach ()
-
-    if (NOT "${target_include_dirs}" MATCHES "NOTFOUND") # Target has direct include dirs
-        list(APPEND include_dirs ${target_include_dirs})
+    if (NOT header_found OR "${header_found}" MATCHES "NOTFOUND")
+        set(_return_var FALSE PARENT_SCOPE)
+    else ()
+        set(_return_var TRUE PARENT_SCOPE)
     endif ()
-
-    _check_header_existance(${_header_we} ${include_dirs} header_found)
-
-    set(_return_var ${header_found} PARENT_SCOPE)
 
 endfunction()
