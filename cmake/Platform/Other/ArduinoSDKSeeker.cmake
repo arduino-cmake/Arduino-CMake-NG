649e8f2c7378b0c5e8e58078cfb511c8659f2980
tellowkrinkle <tellowkrinkle@users.noreply.github.com>
2019-04-22 02:04:27 -0500

    Use `MATCHES "NOTFOUND"` for variable checks

--- cmake/Platform/Other/ArduinoSDKSeeker.cmake
+++ cmake/Platform/Other/ArduinoSDKSeeker.cmake
@@ -58,14 +58,14 @@ function(find_arduino_sdk_bin _return_var)
     else ()
         # Some systems like the Arch Linux arduino package install binaries to /usr/bin
         find_program(avr_gcc_location avr-gcc)
-        if (avr_gcc_location)
-            get_filename_component(avr_gcc_parent ${avr_gcc_location} DIRECTORY)
-            set(${_return_var} "${avr_gcc_parent}" PARENT_SCOPE)
-        else ()
+        if ("${avr_gcc_location}" MATCHES "NOTFOUND")
             string(CONCAT error_message
                     "Couldn't find Arduino bin path - Is it in a non-standard location?" "\n"
                     "If so, please set the ARDUINO_SDK_BIN_PATH CMake-Variable")
             message(FATAL_ERROR ${error_message})
+        else ()
+            get_filename_component(avr_gcc_parent ${avr_gcc_location} DIRECTORY)
+            set(${_return_var} "${avr_gcc_parent}" PARENT_SCOPE)
         endif ()
     endif ()
 
