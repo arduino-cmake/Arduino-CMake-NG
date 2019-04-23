753eca7ecfc9504d1d62ae5c3cb878554b4fbda7
TellowKrinkle <tellowkrinkle@gmail.com>
2019-04-23 02:57:49 -0500

    Directly specify sketchbook dir

--- .travis-fedora-build/run.sh
+++ .travis-fedora-build/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/Arduino-Toolchain.cmake -DAUTO_SET_SKETCHBOOK_PATH=ON ../examples/
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/Arduino-Toolchain.cmake -DARDUINO_CMAKE_SKETCHBOOK_PATH=/root/Arduino ../examples/
 make
 
